6be3771ee8987ceb536faba993bbfdccfe2a1a82
valerijzolotuhin <valerijzolotuhin@C2F7>
2024-05-18 15:09:49 +0300

    Add project files.

---
+++ user_app/LawyerWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace user_app
+{
+    /// <summary>
+    /// Interaction logic for LawyerWindow.xaml
+    /// </summary>
+    public partial class LawyerWindow : Window
+    {
+        public LawyerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
